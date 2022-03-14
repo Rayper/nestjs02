7da6a63bf33682d2a6d1a0504b3deffe907ce35e
Rayper <raihangunawan22@gmail.com>
2022-03-14 12:41:47 +0700

    video 4.6 - 4.10 The Entity, Repository, Querying Criteria

--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,7 +1,22 @@
+import { cp } from "fs";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+// @Entity('event', {name: 'event'})
+@Entity()
 export class Event {
+
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+    
+    @Column()
     description: string;
+    
+    @Column()
     when: Date;
+    
+    @Column()
     address: string;
 }
